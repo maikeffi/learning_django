30dd52d0c638f7df1740c96ba59920be0c85fbbf
maikeffi <maikeffi@gmail.com>
2018-09-16 21:47:09 +0530

    trying some changes

--- pyrunner/views.py
+++ pyrunner/views.py
@@ -1,6 +1,10 @@
+import os
+import fpdf
+from io import StringIO
 from django.shortcuts import render
 from django.http import HttpResponse
 from subprocess import Popen,PIPE
+from reportlab.pdfgen import canvas
 # Create your views here.
 
 def index(request):
@@ -9,7 +13,13 @@ def pyrunner(request):
     if request.method == 'POST':
         info = request.POST['info_name']
         output = runscript(info)
-        print(info)
+        '''response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename = "pythonoutput.pdf"'
+        p = canvas.Canvas(response)
+        p.drawString(100, 200, output)
+        p.showPage()
+        p.save()
+        return response'''
     else:
         info = "failed"
         output = "failed"
@@ -18,9 +28,22 @@ def pyrunner(request):
         'output': output,
       })
 
+
 def runscript(filepath):
     #output = subprocess.call(['python' ,filepath],shell=True)
-    process = Popen(['python',filepath],stdout=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout)
-    return stdout
+    #process = Popen(['python',filepath],stdout=PIPE)
+    #stdout, stderr = process.communicate()
+    #print(stdout)
+    pdf = fpdf.FPDF(format='letter')
+    savepath = str(filepath).replace(".py",".pdf")
+    pdf.add_page()
+    pdf.set_font("Arial", size=8)
+    with Popen(['python3',filepath], stdout=PIPE, bufsize=1,universal_newlines=True) as p, StringIO() as buf:
+        for line in p.stdout:
+            print(line, end='')
+            buf.write(line)
+            pdf.write(5,line)
+        output = buf.getvalue()
+    pdf.output(savepath)
+    return savepath
+
